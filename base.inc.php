91b275c127849346ecb092746bebe113481790d2
Léo Peltier <contact@leo-peltier.fr>
2011-11-05 01:53:31 +0100

    Final get_cctv.

--- base.inc.php
+++ base.inc.php
@@ -12,18 +12,7 @@ class Utils {
 
 
 	public static function getKml() {
-		return array(
-			(object) array(
-				'name'	=> 'Test 1',
-				'lat'	=> 48.92940908174700000,
-				'lng'	=> 2.15374071253510000
-			),
-			(object) array(
-				'name'	=> 'Test 2',
-				'lat'	=> 48.90156923069000000,
-				'lng'	=> 2.35869910815810000
-			),
-		);
+		return unserialize(file_get_contents('db.dat'));
 	}
 
 }
