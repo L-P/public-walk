6f36124ddc8a566dcb15a4a85bfbc1d110a4aff0
Jason <contact@jason-joyez.fr>
2011-11-05 14:26:19 +0100

    Optimize distance.js

--- webroot/js/distance.js
+++ webroot/js/distance.js
@@ -1,10 +1,8 @@
-// https://secure.wikimedia.org/wikipedia/en/wiki/Haversine_formula
-
-function getDistanceFromCoordinates(lat1, lng1, lat2, lng2) {
-    var earthRadius = 6371; // kilometers
+function getDistanceInMetersFromCoordinates(lat1, lng1, lat2, lng2) {
+    // https://secure.wikimedia.org/wikipedia/en/wiki/Haversine_formula
     var deltaLat = (lat2-lat1)*Math.PI/180;
     var deltaLng = (lng2-lng1)*Math.PI/180;
-    var a = Math.sin(deltaLat/2) * Math.sin(deltaLat/2) + Math.sin(deltaLng/2) * Math.sin(deltaLng/2) * Math.cos(lat1*Math.PI/180) * Math.cos(lat2*Math.PI/180);
+    var a = Math.sin(deltaLat/2)*Math.sin(deltaLat/2)+Math.sin(deltaLng/2)*Math.sin(deltaLng/2)*Math.cos(lat1*Math.PI/180)*Math.cos(lat2*Math.PI/180);
     var b = 2*Math.atan2(Math.sqrt(a),Math.sqrt(1-a));
-    return earthRadius*b;
+    return Math.floor(6371000*b);
 }
