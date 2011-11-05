39962d893c50342eac6c96150ea71a70e9ecbf97
Tim <tim@pixelastic.com>
2011-11-05 02:02:55 +0100

    Refactor js code, added addCamera method

--- webroot/js/script.js
+++ webroot/js/script.js
@@ -1,15 +1,32 @@
 $(function main() {
-		var latlng = new google.maps.LatLng(48.85790014566900000, 2.34716868082870000);
-		var myOptions = {
-				zoom: 13,
-				center: latlng,
+		// Default position
+		var userPos = {
+			lat: 48.85790014566900000,
+			lng: 2.34716868082870000
+		}
+
+		// Display main map
+		var mainMapOptions = {
+				zoom: 4,
+				center: new google.maps.LatLng(userPos.lat, userPos.lng),
 				mapTypeId: google.maps.MapTypeId.ROADMAP
 		};
-		var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+		var mainMap = new google.maps.Map(document.getElementById("map_canvas"), mainMapOptions);
 
-		var marker = new google.maps.Marker({
-			position:new google.maps.LatLng(48.85790014566900000, 2.34716868082870000),
-			map: map, 
-			title:"Hello World!"
-		}); 
+		// Add a new marker on the map
+		function addCamera(lat, lng, name) {
+			return new google.maps.Marker({
+				position:new google.maps.LatLng(lat, lng),
+				map: mainMap, 
+				title:name
+			}); 
+		}
+
+		$.ajax({
+			url : 'get_cctv.php',
+			data : userPos,
+			success: function(data) {
+				console.log(data);
+			}
+		});
 });
