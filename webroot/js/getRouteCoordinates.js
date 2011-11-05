c0eb0264d8d3d23c7b71c30a188456ab61005e23
Jason <contact@jason-joyez.fr>
2011-11-05 23:16:06 +0100

    getRouteCoo done

--- webroot/js/getRouteCoordinates.js
+++ webroot/js/getRouteCoordinates.js
@@ -1,14 +1,18 @@
-//need underscore-min.js
+// need underscore.js
 
-var directionsService = new google.maps.DirectionsService();
-
-var directionsRequest = {destination:"48.92940908174700000,2.15374071253510000",origin:"48.89830111115800000,2.35248649422120000",travelMode:google.maps.DirectionsTravelMode.WALKING};
-
-directionsService.route(directionsRequest, function(result, status) {
-    _.each(result.routes[0].legs[0].steps, function(step) {
-        _.each(step.path, function(path) {
-            //Dump all coordinates to console.log
-            console.log(path.toString());
+function getRouteCoordinates(fromLat, fromLng, toLat, toLng) {
+    var directionsService = new google.maps.DirectionsService();
+    var directionsRequest = {
+    destination:new LatLng(toLat, toLng),
+    origin:new LatLng(fromLat, fromLng),
+    travelMode:google.maps.DirectionsTravelMode.WALKING};
+    var yeeeaaaah = new array();
+    directionsService.route(directionsRequest, function(result, status) {
+        _.each(result.routes[0].legs[0].steps, function(step) {
+            _.each(step.path, function(path) {
+                yeeeaaaah.push(path);
+            });
         });
     });
-});
+    return yeeeaaah;
+}
